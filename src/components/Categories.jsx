4851abdf67a3ee65e7d859f7e4376c2dfecad4f1
Pavel <silvesterkot@gmail.com>
2020-07-25 10:56:40 +1000

    sortPopup и роутинг

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Categories() {
+//class Categories extends React.Component {
+//  state = {
+//    activeItem: 0,
+//  };
+//  onSelectItem = (index) => {
+//    this.setState({
+//      activeItem: index,
+//    });
+//  };
+
+//  render() {
+//    const { items, onClickItem } = this.props;
+//    return (
+//      <div>
+//        <div className="categories">
+//          <ul>
+//            <li>Все</li>
+//            {items.map((name, index) => (
+//              <li
+//                className={this.state.activeItem === index ? 'active' : ''}
+//                onClick={() => this.onSelectItem(index)}
+//                key={`${name}_${index}`}>
+//                {name}
+//              </li>
+//            ))}
+//          </ul>
+//        </div>
+//      </div>
+//    );
+//  }
+//}
+
+function Categories({ items, onClick }) {
+  const [activeItem, setActiveItem] = useState(null);
   return (
     <div>
       <div className="categories">
         <ul>
-          <li className="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
+          <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>
+            Все
+          </li>
+          {items &&
+            items.map((name, index) => (
+              <li
+                className={activeItem === index ? 'active' : ''}
+                onClick={() => setActiveItem(index)}
+                key={`${name}_${index}`}>
+                {name}
+              </li>
+            ))}
         </ul>
       </div>
     </div>
